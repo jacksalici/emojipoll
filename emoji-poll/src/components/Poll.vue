ad9276f1b12a18a62a38b07133da9d57cd1a80ce
Giacomo Salici <giambo2000@gmail.com>
2022-08-03 12:42:53 +0200

    there was an idea

--- emoji-poll/src/components/Poll.vue
+++ emoji-poll/src/components/Poll.vue
@@ -2,26 +2,41 @@
 
 <script>
 
+
     export default{
         name: 'PollCore',
     
         data(){
             return{
-            count:0
-            }
-    }
+                count:0,
+                tasks:[
 
-    
+                ],
+                task:"",
+            }
+        },
+
+        methods: {
+                    addEntry(){
+                        this.count++
+                        this.tasks.push(this.task);
+                        this.task=""
+                    },
+
+                }
+
+        
     }
 </script>
 
 <template>
     <div>
-        <input placeholder="add entry">
-        <button @click="count++">OK</button>
-
+        <form @submit.prevent="addEntry">
+            <input placeholder="add entry" v-model="task">
+        </form>
 
         <h1>{{count}}</h1>
+        <p v-for="(task, index) in tasks" :key="index">{{task}}</p>
     </div>
 
 </template>
