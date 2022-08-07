9aa573ebdf4f80ca4cdf64fcfc58a36090c0f438
Giacomo Salici <giambo2000@gmail.com>
2022-08-07 19:19:08 +0200

    emoji polling!

--- emoji-poll/src/components/Poll.vue
+++ emoji-poll/src/components/Poll.vue
@@ -8,20 +8,29 @@
     
         data(){
             return{
-                count:0,
-                tasks:[
-
-                ],
+                tasks:{},
                 task:"",
+                count:0
             }
         },
 
         methods: {
                     addEntry(){
+                        const dict = {desc: this.task, emoji:""}
                         this.count++
-                        this.tasks.push(this.task);
+                        this.getEmoji(this.count)
+                        this.tasks[this.count]= dict;
                         this.task=""
                     },
+
+                    getEmoji(i){
+                        fetch("http://emoji.deta.dev/random/")
+                        .then(response => response.json())
+                        .then(data => {
+                            console.log(data[0])
+                            this.tasks[i]["emoji"]=data[0]
+                            });
+                    }
 
                 }
 
@@ -31,12 +40,26 @@
 
 <template>
     <div>
-        <form @submit.prevent="addEntry">
-            <input placeholder="add entry" v-model="task">
-        </form>
+        
+        <table class="min-w-full border-gray-300">
+            <tr v-for="(task, index) in tasks" :key="index">
+                <td class="whitespace-nowrap px-3 py-4 text-sm">
+                    {{task.emoji}}
+                </td>
+                
+                <td class="whitespace-nowrap px-3 py-4 text-sm">
+                    {{task.desc}}
+                </td>
 
-        <h1>{{count}}</h1>
-        <p v-for="(task, index) in tasks" :key="index">{{task}}</p>
+
+            </tr>
+
+
+        </table>
+        
+        <form class="flex w-full max-w-md flex-col items-center space-y-3 sm:flex-row sm:space-y-0 sm:space-x-3 my-5" @submit.prevent="addEntry">
+        <input type='text' class="w-full rounded-md border bg-gray-50 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:cursor-not-allowed disabled:opacity-50" placeholder="add entry" v-model="task">
+      </form>
     </div>
 
 </template>
