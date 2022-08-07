a65f86897879f290b2ec490edca375e018fa88d3
Giacomo Salici <giambo2000@gmail.com>
2022-08-07 19:18:58 +0200

    tailwindcss

--- emoji-poll/src/App.vue
+++ emoji-poll/src/App.vue
@@ -1,5 +1,14 @@
 <template>
-  <PollCore/>
+  <div class="container mx-auto px-4 py-14 sm:px-6 xl:px-12">
+  <div class="flex flex-col items-center justify-center space-y-6 text-center">
+    <h1 class="text-4xl font-bold tracking-normal sm:text-5xl lg:text-6xl">EmojiPoll</h1>
+    <p class="max-w-screen-sm text-lg text-gray-600 sm:text-2xl">Create a poll using random emoji. Just fill the entries, copy the poll text and paste it on your favorite reaction-featured instant-messaging app.</p>
+      <PollCore/>
+
+</div>
+  </div>
+  
+
 </template>
 
 <script>
@@ -13,13 +22,3 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
